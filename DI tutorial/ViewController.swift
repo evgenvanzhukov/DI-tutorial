f4674d7f8e07e3114869f739a311ba724d35441a
evgen z <evgen-2208@ya.ru>
2023-01-13 14:36:12 +0400

    Initial Commit

---
+++ DI tutorial/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DI tutorial
+//
+//  Created by Evgen on 13.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
